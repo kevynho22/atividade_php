45007f7ac16677d3f3d094e17d53ecb02ade5ff4
kevynho22 <kevyng.ifpr2024@gmail.com>
2025-12-05 11:59:03 -0300

    Add files via upload

---
+++ atividade.php/projetos_jogos/util/Conexao.php
@@ -0,0 +1,33 @@
+<?php
+
+class Conexao
+{
+    public static ?PDO $conexao = null;
+
+    public static function getConexao(): PDO
+    {
+
+        if (self::$conexao == null) {
+            $strCon = "mysql:host=localhost:3306;dbname=bd_jogos";
+            $opcoes = array(
+                // Define o charset da conexão
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+
+                // Define o tipo do erro como exceção
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+
+                // Define o tipo padrão da busca
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            );
+
+            self::$conexao = new PDO(
+                $strCon,
+                "root",          // usuário
+                "bancodedados",  // senha
+                $opcoes
+            );
+        }
+
+        return self::$conexao;
+    }
+}
